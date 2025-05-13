8117c08686fa461babe08460f970d43a7be0ed39
characterMi <abol1385fx@gmail.com>
2025-05-13 20:53:11 +0330

    adding sharp library for next.js Image optimization

--- components/ManageCacheButton.tsx
+++ components/ManageCacheButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useManageCacheModal } from "@/store/useManageCacheModal";
-import { MdOutlineStorage } from "react-icons/md";
+import { GrStorage } from "react-icons/gr";
 
 const ManageCacheButton = () => {
   const openModal = useManageCacheModal((state) => state.onOpen);
@@ -11,8 +11,8 @@ const ManageCacheButton = () => {
       className="flex flex-row h-auto items-center w-full gap-x-[14px] text-sm font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1"
       onClick={openModal}
     >
-      <MdOutlineStorage size={24} aria-hidden />
-      <p className="truncate">Manage Cache</p>
+      <GrStorage size={22} aria-hidden />
+      <p className="truncate ml-[2px]">Manage Cache</p>
     </button>
   );
 };
