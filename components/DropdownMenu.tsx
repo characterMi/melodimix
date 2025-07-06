434315dbc9dcafcf8205c095efb4ca4fde5bf2d7
characterMi <abol1385fx@gmail.com>
2025-07-06 15:40:18 +0330

    lazy loading radix components

--- components/DropdownMenu.tsx
+++ components/DropdownMenu.tsx
@@ -1,8 +1,31 @@
 "use client";
 
-import * as DropdownMenuFromRadix from "@radix-ui/react-dropdown-menu";
+import type { DropdownMenuItemProps } from "@radix-ui/react-dropdown-menu";
+import dynamic from "next/dynamic";
 import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
+
+const Root = dynamic(() =>
+  import("@radix-ui/react-dropdown-menu").then((mod) => mod.Root)
+);
+const Trigger = dynamic(() =>
+  import("@radix-ui/react-dropdown-menu").then((mod) => mod.Trigger)
+);
+const Content = dynamic(() =>
+  import("@radix-ui/react-dropdown-menu").then((mod) => mod.Content)
+);
+const Group = dynamic(() =>
+  import("@radix-ui/react-dropdown-menu").then((mod) => mod.Group)
+);
+const Label = dynamic(() =>
+  import("@radix-ui/react-dropdown-menu").then((mod) => mod.Label)
+);
+const Item = dynamic(() =>
+  import("@radix-ui/react-dropdown-menu").then((mod) => mod.Item)
+);
+const Separator = dynamic(() =>
+  import("@radix-ui/react-dropdown-menu").then((mod) => mod.Separator)
+);
 
 type Props = {
   triggerProps: {
@@ -19,8 +42,8 @@ type Props = {
 
 const DropdownMenu = ({ triggerProps, contentProps, children }: Props) => {
   return (
-    <DropdownMenuFromRadix.Root modal={false}>
-      <DropdownMenuFromRadix.Trigger
+    <Root modal={false}>
+      <Trigger
         aria-label={triggerProps.label}
         className={twMerge(
           "cursor-pointer hover:opacity-75 focus-visible:opacity-75 outline-none transition",
@@ -29,21 +52,21 @@ const DropdownMenu = ({ triggerProps, contentProps, children }: Props) => {
         tabIndex={triggerProps.tabIndex}
       >
         {triggerProps.element}
-      </DropdownMenuFromRadix.Trigger>
+      </Trigger>
 
-      <DropdownMenuFromRadix.Content
+      <Content
         className={twMerge(
           "bg-neutral-900 border border-neutral-700 p-4 rounded-lg shadow-2xl flex flex-col z-[2]",
           contentProps?.className
         )}
       >
         {children}
-      </DropdownMenuFromRadix.Content>
-    </DropdownMenuFromRadix.Root>
+      </Content>
+    </Root>
   );
 };
 
-DropdownMenu.Group = DropdownMenuFromRadix.Group;
+DropdownMenu.Group = Group;
 DropdownMenu.Label = ({
   children,
   className,
@@ -51,24 +74,17 @@ DropdownMenu.Label = ({
   children: React.ReactNode;
   className?: string;
 }) => (
-  <DropdownMenuFromRadix.Label
-    className={twMerge("font-bold text-xl", className)}
-  >
-    {children}
-  </DropdownMenuFromRadix.Label>
+  <Label className={twMerge("font-bold text-xl", className)}>{children}</Label>
 );
-DropdownMenu.Item = forwardRef<
-  HTMLDivElement,
-  DropdownMenuFromRadix.DropdownMenuItemProps
->((props, ref) => (
-  <DropdownMenuFromRadix.Item {...props} ref={ref}>
-    {props.children}
-  </DropdownMenuFromRadix.Item>
-));
+DropdownMenu.Item = forwardRef<HTMLDivElement, DropdownMenuItemProps>(
+  (props, ref) => (
+    <Item {...props} ref={ref}>
+      {props.children}
+    </Item>
+  )
+);
 DropdownMenu.Separator = ({ className }: { className?: string }) => (
-  <DropdownMenuFromRadix.Separator
-    className={twMerge("h-[2px] bg-neutral-800", className)}
-  />
+  <Separator className={twMerge("h-[2px] bg-neutral-800", className)} />
 );
 
 export default DropdownMenu;
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,27 @@
-import * as Dialog from "@radix-ui/react-dialog";
+import dynamic from "next/dynamic";
 import { IoMdClose } from "react-icons/io";
+
+const Root = dynamic(() =>
+  import("@radix-ui/react-dialog").then((mod) => mod.Root)
+);
+const Portal = dynamic(() =>
+  import("@radix-ui/react-dialog").then((mod) => mod.Portal)
+);
+const Overlay = dynamic(() =>
+  import("@radix-ui/react-dialog").then((mod) => mod.Overlay)
+);
+const Content = dynamic(() =>
+  import("@radix-ui/react-dialog").then((mod) => mod.Content)
+);
+const Title = dynamic(() =>
+  import("@radix-ui/react-dialog").then((mod) => mod.Title)
+);
+const Description = dynamic(() =>
+  import("@radix-ui/react-dialog").then((mod) => mod.Description)
+);
+const Close = dynamic(() =>
+  import("@radix-ui/react-dialog").then((mod) => mod.Close)
+);
 
 interface Props {
   isOpen: boolean;
@@ -17,32 +39,30 @@ const Modal = ({
   title,
 }: Props) => {
   return (
-    <Dialog.Root open={isOpen} defaultOpen={isOpen} onOpenChange={handleChange}>
-      <Dialog.Portal>
-        <Dialog.Overlay className="bg-neutral-900/90 backdrop-blur-sm fixed inset-0 z-50 hidden md:block" />
+    <Root open={isOpen} defaultOpen={isOpen} onOpenChange={handleChange}>
+      <Portal>
+        <Overlay className="bg-neutral-900/90 backdrop-blur-sm fixed inset-0 z-50 hidden md:block" />
 
-        <Dialog.Content className="fixed drop-shadow-md border border-neutral-700 top-1/2 left-1/2 max-h-full h-full md:h-auto md:max-h-[85vh] w-full md:w-[90vw] md:max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-neutral-800 p-[25px] focus:outline-none z-[100] overflow-y-auto">
-          <Dialog.Title className="text-xl text-center font-bold mb-4">
-            {title}
-          </Dialog.Title>
+        <Content className="fixed drop-shadow-md border border-neutral-700 top-1/2 left-1/2 max-h-full h-full md:h-auto md:max-h-[85vh] w-full md:w-[90vw] md:max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-neutral-800 p-[25px] focus:outline-none z-[100] overflow-y-auto">
+          <Title className="text-xl text-center font-bold mb-4">{title}</Title>
 
-          <Dialog.Description className="mb-5 text-sm leading-normal text-center">
+          <Description className="mb-5 text-sm leading-normal text-center">
             {description}
-          </Dialog.Description>
+          </Description>
 
           <div>{children}</div>
 
-          <Dialog.Close asChild>
+          <Close asChild>
             <button
               className="text-neutral-400 hover:text-white absolute top-[10px] inline-flex h-[25px] w-[25px] right-[10px] items-center justify-center appearance-none rounded-full"
               aria-label="Close"
             >
               <IoMdClose aria-hidden />
             </button>
-          </Dialog.Close>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+          </Close>
+        </Content>
+      </Portal>
+    </Root>
   );
 };
 export default Modal;
