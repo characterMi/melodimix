3fa1e66f8b86e06b83dcba3eb0f8c1a0210b1456
characterMi <abol1385fx@gmail.com>
2025-09-05 12:27:45 +0330

    fixing the offline refresh token request problem

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,4 +1,21 @@
 import type { Database } from "@/types/db";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export const supabaseClient = createClientComponentClient<Database>();
+export const supabaseClient = createClientComponentClient<Database>({
+  options: {
+    global: {
+      fetch: (input, init) => {
+        if (
+          input
+            .toString()
+            .endsWith("/auth/v1/token?grant_type=refresh_token") &&
+          !navigator.onLine
+        ) {
+          return Promise.reject("You're offline, cannot validate the token.");
+        }
+
+        return fetch(input, init);
+      },
+    },
+  },
+});
--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -2,7 +2,7 @@
 
 import { supabaseClient as supabase } from "@/lib/supabaseClient";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { useInsertionEffect, useState } from "react";
+import { useState } from "react";
 
 interface Props {
   children: React.ReactNode;
@@ -10,22 +10,6 @@ interface Props {
 
 export const SupabaseProvider = ({ children }: Props) => {
   const [supabaseClient] = useState(supabase);
-
-  useInsertionEffect(() => {
-    const handleOnline = () => supabaseClient.auth.startAutoRefresh();
-
-    const handleOffline = () => supabaseClient.auth.stopAutoRefresh();
-
-    if (!navigator.onLine) handleOffline();
-
-    window.addEventListener("online", handleOnline);
-    window.addEventListener("offline", handleOffline);
-
-    return () => {
-      window.removeEventListener("online", handleOnline);
-      window.removeEventListener("offline", handleOffline);
-    };
-  }, [supabaseClient]);
 
   return (
     <SessionContextProvider supabaseClient={supabaseClient}>
