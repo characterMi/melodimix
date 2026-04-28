ed59fe73265f2eac24bc2ae088d16eefff73decb
characterMi <abol1385fx@gmail.com>
2026-04-28 18:57:20 +0330

    creating the Draggable component in MobilePlayer

--- components/MobilePlayer.tsx
+++ components/MobilePlayer.tsx
@@ -32,13 +32,6 @@ const MobilePlayer = ({
   closeMobilePlayerButton: RefObject<HTMLButtonElement>;
   isMobilePlayerOpen: boolean;
 }) => {
-  const { current: onMobilePlayerClose } = useRef(() => window.history.back());
-  const dragPosData = useRef({
-    start: 0,
-    current: 0,
-    isDragging: false,
-    hasContainerScrolled: false,
-  });
   const contentContainer = useRef<HTMLDivElement>(null);
   const mobilePlayerRef = useRef<HTMLDivElement>(null);
   const songImage = useLoadImage(song);
@@ -49,10 +42,110 @@ const MobilePlayer = ({
     (e: KeyboardEvent<HTMLDivElement>) => {
       if (!isMobilePlayerOpen || e.key !== "Escape") return;
 
-      onMobilePlayerClose();
+      window.history.back();
     },
     [isMobilePlayerOpen]
   );
+
+  return createPortal(
+    // @ts-ignore
+    <div
+      className={twMerge(
+        "w-full h-sm-screen bg-gradient-to-t from-black to-emerald-800 fixed top-0 left-0 z-[100] overflow-hidden sm:hidden",
+        isMobilePlayerOpen ? "translate-y-0" : "translate-y-full",
+        !shouldReduceMotion && "transition-transform duration-300"
+      )}
+      ref={mobilePlayerRef}
+      aria-hidden={!isMobilePlayerOpen}
+      id="mobile-player"
+      role="dialog"
+      aria-label="Mobile player"
+      aria-modal
+      onKeyDown={onKeyDown}
+      {...(isMobilePlayerOpen ? {} : { inert: "true" })}
+    >
+      {/* Backdrop */}
+      <SongCover
+        src={songImage || "/images/song.png"}
+        alt={""}
+        aria-hidden
+        width={1}
+        height={1}
+        className={twMerge(
+          "size-full absolute z-[-1] opacity-50",
+          isMobilePlayerOpen && "blur-lg"
+        )}
+        renderErrorFallback={false}
+      />
+      <div
+        className="absolute top-0 left-0 w-full h-full z-[-1] bg-gradient-to-t from-black from-0% via-transparent via-75% to-emerald-800 to-100%"
+        aria-hidden
+      />
+
+      <div
+        className="size-full flex flex-col justify-between items-center relative overflow-x-hidden overflow-y-auto"
+        ref={contentContainer}
+      >
+        <div className="w-full flex justify-between items-center p-6 xss:p-8">
+          <button
+            ref={closeMobilePlayerButton}
+            className={twMerge(
+              "hover:opacity-50 z-[1] focus-visible:opacity-50 outline-none",
+              !shouldReduceMotion && "transition-opacity"
+            )}
+            onClick={() => window.history.back()}
+            aria-label="Close the mobile player"
+          >
+            <GoArrowLeft size={32} aria-hidden />
+          </button>
+
+          <h2 className="text-2xl text-neutral-200 font-thin">Now playing</h2>
+
+          <SongOptions
+            song={song}
+            songUrl={songUrl}
+            triggerClasses="rotate-90 z-[1]"
+            triggerSize={30}
+          />
+        </div>
+
+        <div className="relative min-h-[250px] max-w-[90vw] aspect-square rounded-xl overflow-hidden bg-black mx-6 xss:mx-8">
+          <SongCover
+            src={songImage || "/images/song.png"}
+            alt={`Album cover for ${song.title}`}
+            width={1000}
+            height={1000}
+            className="h-full shadow-2xl"
+          />
+        </div>
+
+        <div className="w-full flex flex-col gap-4 pb-14 p-6 xss:p-8">
+          {children}
+        </div>
+
+        <Draggable
+          mobilePlayerRef={mobilePlayerRef}
+          contentContainer={contentContainer}
+        />
+      </div>
+    </div>,
+    document.body
+  );
+};
+
+const Draggable = ({
+  mobilePlayerRef,
+  contentContainer,
+}: {
+  mobilePlayerRef: RefObject<HTMLDivElement>;
+  contentContainer: RefObject<HTMLDivElement>;
+}) => {
+  const dragPosData = useRef({
+    start: 0,
+    current: 0,
+    isDragging: false,
+    hasContainerScrolled: false,
+  });
 
   const onDragStart = useCallback((e: TouchEvent | MouseEvent) => {
     if (!mobilePlayerRef.current) return;
@@ -107,98 +200,20 @@ const MobilePlayer = ({
     mobilePlayerRef.current.style.removeProperty("transform");
 
     if (dragPosData.current.current > window.innerHeight / 3) {
-      onMobilePlayerClose();
+      window.history.back();
     }
   }, []);
 
-  return createPortal(
-    // @ts-ignore
+  return (
     <div
-      className={twMerge(
-        "w-full h-sm-screen bg-gradient-to-t from-black to-emerald-800 fixed top-0 left-0 z-[100] overflow-hidden sm:hidden",
-        isMobilePlayerOpen ? "translate-y-0" : "translate-y-full",
-        !shouldReduceMotion && "transition-transform duration-300"
-      )}
-      ref={mobilePlayerRef}
-      aria-hidden={!isMobilePlayerOpen}
-      id="mobile-player"
-      role="dialog"
-      aria-label="Mobile player"
-      aria-modal
-      onKeyDown={onKeyDown}
-      {...(isMobilePlayerOpen ? {} : { inert: "true" })}
-    >
-      {/* Backdrop */}
-      <SongCover
-        src={songImage || "/images/song.png"}
-        alt={""}
-        aria-hidden
-        width={1}
-        height={1}
-        className={twMerge(
-          "size-full absolute z-[-1] opacity-50",
-          isMobilePlayerOpen && "blur-lg"
-        )}
-        renderErrorFallback={false}
-      />
-      <div
-        className="absolute top-0 left-0 w-full h-full z-[-1] bg-gradient-to-t from-black from-0% via-transparent via-75% to-emerald-800 to-100%"
-        aria-hidden
-      />
-
-      <div
-        className="size-full flex flex-col justify-between items-center relative overflow-x-hidden overflow-y-auto"
-        ref={contentContainer}
-      >
-        <div className="w-full flex justify-between items-center p-6 xss:p-8">
-          <button
-            ref={closeMobilePlayerButton}
-            className={twMerge(
-              "hover:opacity-50 z-[1] focus-visible:opacity-50 outline-none",
-              !shouldReduceMotion && "transition-opacity"
-            )}
-            onClick={onMobilePlayerClose}
-            aria-label="Close the mobile player"
-          >
-            <GoArrowLeft size={32} aria-hidden />
-          </button>
-
-          <h2 className="text-2xl text-neutral-200 font-thin">Now playing</h2>
-
-          <SongOptions
-            song={song}
-            songUrl={songUrl}
-            triggerClasses="rotate-90 z-[1]"
-            triggerSize={30}
-          />
-        </div>
-
-        <div className="relative min-h-[250px] max-w-[90vw] aspect-square rounded-xl overflow-hidden bg-black mx-6 xss:mx-8">
-          <SongCover
-            src={songImage || "/images/song.png"}
-            alt={`Album cover for ${song.title}`}
-            width={1000}
-            height={1000}
-            className="h-full shadow-2xl"
-          />
-        </div>
-
-        <div className="w-full flex flex-col gap-4 pb-14 p-6 xss:p-8">
-          {children}
-        </div>
-
-        <div
-          onPointerDown={onDragStart}
-          onTouchMove={onDrag}
-          onMouseMove={onDrag}
-          onTouchEnd={onDragEnd}
-          onMouseUp={onDragEnd}
-          className="size-full absolute top-0 left-0 cursor-grab"
-          aria-label="Drag to close the Mobile player"
-        />
-      </div>
-    </div>,
-    document.body
+      onPointerDown={onDragStart}
+      onTouchMove={onDrag}
+      onMouseMove={onDrag}
+      onTouchEnd={onDragEnd}
+      onMouseUp={onDragEnd}
+      className="size-full absolute top-0 left-0 cursor-grab"
+      aria-label="Drag to close the Mobile player"
+    />
   );
 };
 
